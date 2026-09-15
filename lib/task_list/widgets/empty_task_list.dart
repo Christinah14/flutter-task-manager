679b8e1a1094890b0af38add1a894300ff7c5eb6
Christinah14 <mutswaletswalec@tihsa.co.za>
2026-09-15 07:23:11 +0200

    Empty task list initial state

---
+++ lib/task_list/widgets/empty_task_list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class EmptyTaskList extends StatelessWidget {
+  const EmptyTaskList({super.key});
+
+  //instance where there is no task
+  // empty state
+  @override
+  Widget build(BuildContext context) {
+    //the child will be in the center
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+
+        //display multiple widget
+        children: [
+          Icon(Icons.task_alt, size: 64, color: Colors.grey),
+
+          SizedBox(height: 16),
+          Text(
+            'No task yet',
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+          ),
+
+          SizedBox(height: 8),
+          Text(
+            'Add a text to get started',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ],
+      ),
+    );
+  }
+}
